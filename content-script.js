9c5325657544f58c4c44bff1bed635ea0a779391
WesselvanDam <wesselvandam@hotmail.com>
2024-06-14 01:28:11 +0200

    fix: change logic to detect  BrightSpace's new structure

--- content-script.js
+++ content-script.js
@@ -1,31 +1,52 @@
 // Function that detects content frames and injects the button
 function mainLoop() {
-  var element = document
-    .getElementsByTagName("d2l-iframe-wrapper-for-react")
-    .item(0);
+  // Get the source attribute of the first child of the 'topid-display' element
+  var source = null;
+  try {
+    source = document
+      .getElementsByClassName("topic-display")
+      .item(0)
+      .firstElementChild.getAttribute("src");
+  } finally {
+    console.log("Source: " + source)
+  }
+
   var newTabButtonList = document.getElementsByClassName("open-in-new-tab-btn");
-  if (element == null && newTabButtonList.length > 0) {
+  console.log("Button list: " + newTabButtonList.length);
+  // No source, but button does not exist: do nothing
+  if (source == null && newTabButtonList.length == 0) {
+    return;
+  }
+  // No source, but button exists: hide button
+  if (source == null && newTabButtonList.length > 0) {
     newTabButtonList.item(0).style.visibility = "hidden";
     return;
   }
+  // Source exists and button exists: show button
   if (newTabButtonList.length > 0) {
     newTabButtonList.item(0).style.visibility = "visible";
     return;
   }
+  // Source exists and button does not exist: insert button
   var buttonTray = document
     .getElementsByClassName("header-button-tray")
     .item(0);
   if (buttonTray == null) return;
+  insertButton(buttonTray, source);
+}
+
+function insertButton(buttonTray, source) {
   buttonTray.insertAdjacentHTML(
     "beforeend",
     `
       <button class="open-in-new-tab-btn" onclick=
-        "window.open(document.getElementsByTagName('d2l-iframe-wrapper-for-react').item(0).getAttribute('src'))">
+        "window.open('${source}')">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>
       </button>
     `
   );
 }
+
 // Initial run
 setTimeout(function () {
   mainLoop();
